755b60d268a3d0e81732b4ea075441633dc86226
LeRoiDesKiwis <lekiwidu17@gmail.com>
2025-07-13 19:06:55 +0200

    fix(xofakindrule): migrate some internal methods to TestUtil

--- src/test/java/fr/leroideskiwis/poker/handrules/XOfAKindRuleTest.java
+++ src/test/java/fr/leroideskiwis/poker/handrules/XOfAKindRuleTest.java
@@ -1,6 +1,7 @@
 package fr.leroideskiwis.poker.handrules;
 
 import fr.leroideskiwis.poker.*;
+import fr.leroideskiwis.poker.util.TestUtil;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -11,51 +12,19 @@ class XOfAKindRuleTest {
 
     private XOfAKindRule xOfAKindRule = new XOfAKindRule();
 
-    private Hand createPair(){
-        return new Hand(new Card(Rank.ACE, Suit.HEARTS),
-                        new Card(Rank.ACE, Suit.SPADES),
-                        new Card(Rank.TWO, Suit.DIAMONDS),
-                        new Card(Rank.THREE, Suit.CLUBS),
-                        new Card(Rank.FOUR, Suit.HEARTS));
-    }
-
-    private Hand createThreeOfAKind() {
-        return new Hand(new Card(Rank.ACE, Suit.HEARTS),
-                        new Card(Rank.ACE, Suit.SPADES),
-                        new Card(Rank.ACE, Suit.DIAMONDS),
-                        new Card(Rank.TWO, Suit.CLUBS),
-                        new Card(Rank.THREE, Suit.HEARTS));
-    }
-
-    private Hand createHandWithNoPairs() {
-        return new Hand(new Card(Rank.ACE, Suit.HEARTS),
-                        new Card(Rank.KING, Suit.SPADES),
-                        new Card(Rank.QUEEN, Suit.DIAMONDS),
-                        new Card(Rank.JACK, Suit.CLUBS),
-                        new Card(Rank.TEN, Suit.HEARTS));
-    }
-
-    private Hand createHandWithFourOfAKind() {
-        return new Hand(new Card(Rank.ACE, Suit.HEARTS),
-                        new Card(Rank.ACE, Suit.SPADES),
-                        new Card(Rank.ACE, Suit.DIAMONDS),
-                        new Card(Rank.ACE, Suit.CLUBS),
-                        new Card(Rank.TWO, Suit.HEARTS));
-    }
-
     @Test
     void testNoXOfAKind() {
-        Hand hand = createHandWithNoPairs();
+        Hand hand = TestUtil.createHighCardHand();
         Optional<EvaluatedRule> result = xOfAKindRule.evaluate(hand);
         assertFalse(result.isPresent(), "Expected no X of a kind in hand with no pairs");
-        hand = createPair();
+        hand = TestUtil.createPairHand();
         result = xOfAKindRule.evaluate(hand);
         assertFalse(result.isPresent(), "Expected no X of a kind in hand with only a pair");
     }
 
     @Test
     void testThreeOfAKind() {
-        Hand hand = createThreeOfAKind();
+        Hand hand = TestUtil.createThreeOfAKindHand();
         Optional<EvaluatedRule> result = xOfAKindRule.evaluate(hand);
         assertTrue(result.isPresent(), "Expected three of a kind to be detected");
         assertTrue(result.get().isPokerHand(PokerHand.THREE_OF_A_KIND), "Expected hand to be evaluated as THREE_OF_A_KIND");
@@ -64,7 +33,7 @@ class XOfAKindRuleTest {
 
     @Test
     void testFourOfAKind() {
-        Hand hand = createHandWithFourOfAKind();
+        Hand hand = TestUtil.createFourOfAKindHand();
         Optional<EvaluatedRule> result = xOfAKindRule.evaluate(hand);
         assertTrue(result.isPresent(), "Expected four of a kind to be detected");
         assertTrue(result.get().isPokerHand(PokerHand.FOUR_OF_A_KIND), "Expected hand to be evaluated as FOUR_OF_A_KIND");
