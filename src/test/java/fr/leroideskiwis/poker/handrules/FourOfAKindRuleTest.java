7542a9d76009c72efd88ddfd18c4135c8fe735db
LeRoiDesKiwis <lekiwidu17@gmail.com>
2025-07-13 19:06:55 +0200

    fix(xofakind): separate three and four of a kind

---
+++ src/test/java/fr/leroideskiwis/poker/handrules/FourOfAKindRuleTest.java
@@ -0,0 +1,41 @@
+package fr.leroideskiwis.poker.handrules;
+
+import fr.leroideskiwis.poker.*;
+import fr.leroideskiwis.poker.util.TestUtil;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FourOfAKindRuleTest {
+
+    private FourOfAKindRule fourofAKindRule = new FourOfAKindRule();
+
+    @Test
+    void testNoXOfAKind() {
+        Hand hand = TestUtil.createHighCardHand();
+        Optional<EvaluatedRule> result = fourofAKindRule.evaluate(hand);
+        assertFalse(result.isPresent(), "Expected no X of a kind in hand with no pairs");
+        hand = TestUtil.createPairHand();
+        result = fourofAKindRule.evaluate(hand);
+        assertFalse(result.isPresent(), "Expected no X of a kind in hand with only a pair");
+    }
+
+    @Test
+    void testThreeOfAKind() {
+        Hand hand = TestUtil.createThreeOfAKindHand();
+        Optional<EvaluatedRule> result = fourofAKindRule.evaluate(hand);
+        assertFalse(result.isPresent(), "Expected three of a kind to be detected");
+    }
+
+    @Test
+    void testFourOfAKind() {
+        Hand hand = TestUtil.createFourOfAKindHand();
+        Optional<EvaluatedRule> result = fourofAKindRule.evaluate(hand);
+        assertTrue(result.isPresent(), "Expected four of a kind to be detected");
+        assertTrue(result.get().isPokerHand(PokerHand.FOUR_OF_A_KIND), "Expected hand to be evaluated as FOUR_OF_A_KIND");
+        assertEquals(new Card(Rank.TWO), result.get().bestCard);
+    }
+
+}
--- src/test/java/fr/leroideskiwis/poker/handrules/ThreeOfAKindRuleTest.java
+++ src/test/java/fr/leroideskiwis/poker/handrules/ThreeOfAKindRuleTest.java
@@ -1,6 +1,9 @@
 package fr.leroideskiwis.poker.handrules;
 
-import fr.leroideskiwis.poker.*;
+import fr.leroideskiwis.poker.Card;
+import fr.leroideskiwis.poker.Hand;
+import fr.leroideskiwis.poker.PokerHand;
+import fr.leroideskiwis.poker.Rank;
 import fr.leroideskiwis.poker.util.TestUtil;
 import org.junit.jupiter.api.Test;
 
@@ -8,36 +11,26 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class XOfAKindRuleTest {
+public class ThreeOfAKindRuleTest {
 
-    private XOfAKindRule xOfAKindRule = new XOfAKindRule();
+    private ThreeOfAKindRule threeOfAKindRule = new ThreeOfAKindRule();
 
     @Test
     void testNoXOfAKind() {
         Hand hand = TestUtil.createHighCardHand();
-        Optional<EvaluatedRule> result = xOfAKindRule.evaluate(hand);
+        Optional<EvaluatedRule> result = threeOfAKindRule.evaluate(hand);
         assertFalse(result.isPresent(), "Expected no X of a kind in hand with no pairs");
         hand = TestUtil.createPairHand();
-        result = xOfAKindRule.evaluate(hand);
+        result = threeOfAKindRule.evaluate(hand);
         assertFalse(result.isPresent(), "Expected no X of a kind in hand with only a pair");
     }
 
     @Test
     void testThreeOfAKind() {
         Hand hand = TestUtil.createThreeOfAKindHand();
-        Optional<EvaluatedRule> result = xOfAKindRule.evaluate(hand);
+        Optional<EvaluatedRule> result = threeOfAKindRule.evaluate(hand);
         assertTrue(result.isPresent(), "Expected three of a kind to be detected");
         assertTrue(result.get().isPokerHand(PokerHand.THREE_OF_A_KIND), "Expected hand to be evaluated as THREE_OF_A_KIND");
         assertEquals(new Card(Rank.TWO), result.get().bestCard);
     }
-
-    @Test
-    void testFourOfAKind() {
-        Hand hand = TestUtil.createFourOfAKindHand();
-        Optional<EvaluatedRule> result = xOfAKindRule.evaluate(hand);
-        assertTrue(result.isPresent(), "Expected four of a kind to be detected");
-        assertTrue(result.get().isPokerHand(PokerHand.FOUR_OF_A_KIND), "Expected hand to be evaluated as FOUR_OF_A_KIND");
-        assertEquals(new Card(Rank.TWO), result.get().bestCard);
-    }
-
 }
